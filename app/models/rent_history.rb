80688d62eb193e60777cecf0fc7112fbbb78e48a
David Jones <dgjones@gmail.com>
2013-12-07 12:14:18 -0800

    remove comment that isn't needed

--- app/models/rent_history.rb
+++ app/models/rent_history.rb
@@ -6,10 +6,6 @@ class RentHistory
     all_data = CSV.read("data/mean-rents-by-ta.csv")
 
     raise all_data.inspect
-    # TODO
-    # return hash of rent in this format:
-    # {:year => rent_amount}
-    # e.g. {:2001 => 32.12, :2002 => 34.27 }
   end
 
 end
